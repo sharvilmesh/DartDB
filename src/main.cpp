31894105faea3acb166926c66d513cc29bd94030
sharvilmesh <sharvilmeshram3@gmail.com>
2026-09-07 22:55:57 +0530

    ADD EXISTS command

--- src/main.cpp
+++ src/main.cpp
@@ -8,23 +8,35 @@ int main() {
     std::string command, key, value;
 
     while (std::cin >> command) {
+
         if (command == "SET") {
             std::cin >> key >> value;
             db[key] = value;
             std::cout << "OK\n";
         }
+
         else if (command == "GET") {
             std::cin >> key;
 
             if (db.count(key))
                 std::cout << db[key] << "\n";
-                else if (command == "DEL") {
-                    std::cin >> key;
-                    db.erase(key);
-                    std::cout << "OK\n";
-                }
             else
                 std::cout << "(nil)\n";
+        }
+
+        else if (command == "DEL") {
+            std::cin >> key;
+            db.erase(key);
+            std::cout << "OK\n";
+        }
+
+        else if (command == "EXISTS") {
+            std::cin >> key;
+
+            if (db.count(key))
+                std::cout << "YES\n";
+            else
+                std::cout << "NO\n";
         }
 
         std::cout << "DartDB> ";
