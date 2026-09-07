c1d010ac45e36d505a3124defdaf0a8a8e71c11a
sharvilmesh <sharvilmeshram3@gmail.com>
2026-09-07 22:29:31 +0530

    Add basic key value storage

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <unordered_map>
+#include <string>
 
 int main() {
-    std::cout << "DartDB is starting..." << std::endl;
+    std::unordered_map<std::string, std::string> db;
+
+    db["name"] = "Sharvil";
+
+    std::cout << "name = " << db["name"] << std::endl;
+
     return 0;
 }
